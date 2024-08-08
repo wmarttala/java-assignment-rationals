da07435dc5c5358188d81a322158c7989a9dd765
Will Marttala <wmarttala123@gmail.com>
2024-08-08 16:12:03 -0500

    Initial commit

---
+++ src/main/java/com/cooksys/ftd/assignments/objects/Rational.java
@@ -0,0 +1,75 @@
+package com.cooksys.ftd.assignments.objects;
+
+import com.cooksys.ftd.assignments.objects.util.MissingImplementationException;
+
+public class Rational implements IRational {
+    /**
+     * Constructor for rational values of the type:
+     * <p>
+     * `numerator / denominator`
+     * <p>
+     * No simplification of the numerator/denominator pair should occur in this method.
+     *
+     * @param numerator   the numerator of the rational value
+     * @param denominator the denominator of the rational value
+     * @throws IllegalArgumentException if the given denominator is 0
+     */
+    public Rational(int numerator, int denominator) throws IllegalArgumentException {
+        throw new MissingImplementationException();
+    }
+
+    /**
+     * @return the numerator of this rational number
+     */
+    @Override
+    public int getNumerator() {
+        throw new MissingImplementationException();
+    }
+
+    /**
+     * @return the denominator of this rational number
+     */
+    @Override
+    public int getDenominator() {
+        throw new MissingImplementationException();
+    }
+
+    /**
+     * Specializable constructor to take advantage of shared code between Rational and SimplifiedRational
+     * <p>
+     * Essentially, this method allows us to implement most of IRational methods directly in the interface while
+     * preserving the underlying type
+     *
+     * @param numerator the numerator of the rational value to construct
+     * @param denominator the denominator of the rational value to construct
+     * @return the constructed rational value
+     * @throws IllegalArgumentException if the given denominator is 0
+     */
+    @Override
+    public Rational construct(int numerator, int denominator) throws IllegalArgumentException {
+        throw new MissingImplementationException();
+    }
+
+    /**
+     * @param obj the object to check this against for equality
+     * @return true if the given obj is a rational value and its
+     * numerator and denominator are equal to this rational value's numerator and denominator,
+     * false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        throw new MissingImplementationException();
+    }
+
+    /**
+     * If this is positive, the string should be of the form `numerator/denominator`
+     * <p>
+     * If this is negative, the string should be of the form `-numerator/denominator`
+     *
+     * @return a string representation of this rational value
+     */
+    @Override
+    public String toString() {
+        throw new MissingImplementationException();
+    }
+}
